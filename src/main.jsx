17dcf4e28754e23f8b25c2cc617f0ff0c2a31a63
Luis Mariano Lopez lena <luismllv89@gmail.com>
2022-11-10 14:09:25 -0600

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import ReactDOM from 'react-dom/client'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomeHook } from './02-useEffect/FormWithCustomeHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks' 
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks' 
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   // </React.StrictMode>
 )
