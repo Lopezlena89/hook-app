2686a151dc6b2a0898329a734fa0b7aaa25809a9
Luis Mariano Lopez lena <luismllv89@gmail.com>
2022-11-10 13:53:39 -0600

    examples

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,47 @@
+import { useCounter,useFetch} from "../hooks";
+import { LoadingQuote,Quote } from "./";
+
+
+
+export const MultipleCustomHooks = () => {
+
+  const  {increment,counter} = useCounter(1);
+
+  const {data,isLoading,hasError} = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+
+  const {author,quote} = !!data && data[0];
+
+  // if(isLoading){
+  //   return(
+  //     <h1>Loading...</h1>
+  //   )
+  // }
+
+  return (
+    <>
+        <h1>BreakingBad Quotes</h1>
+        <hr />
+
+        {
+          (isLoading)
+           ? <LoadingQuote/>
+           :<Quote  quote={quote} author={author }/>
+        }
+
+
+        <button 
+          className="btn btn-primary"
+          disabled={isLoading}
+          onClick={()=>increment()}
+        >
+            
+          Next Quote
+        </button>
+
+        
+        
+
+
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 // import { HookApp } from './01-useState/HookApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
-import { FormWithCustomeHook } from './02-useEffect/FormWithCustomeHook'
+// import { FormWithCustomeHook } from './02-useEffect/FormWithCustomeHook'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks' 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <FormWithCustomeHook />
+    <MultipleCustomHooks />
   // </React.StrictMode>
 )
