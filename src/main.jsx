ced196c957dbbf1593af0e777294bd369ca1df31
Luis Mariano Lopez lena <luismllv89@gmail.com>
2022-11-10 15:18:59 -0600

    memos

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,15 @@ import ReactDOM from 'react-dom/client'
 // import { FormWithCustomeHook } from './02-useEffect/FormWithCustomeHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks' 
 // import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
+// import { Layout } from './05-useLayoutEffect/Layout'
+// import { Memorize } from './06-memos/Memorize'
+// import { MemoHook } from './06-memos/MemoHook'
+import { CallBackHook } from './06-memos/CallBackHook'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Layout />
+    <CallBackHook />
   // </React.StrictMode>
 )
