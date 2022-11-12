31783ce6f624c0a078fb92eb95721f04ab1efcc9
Luis Mariano Lopez lena <luismllv89@gmail.com>
2022-11-12 13:11:58 -0600

    useReducer

---
+++ src/08-useReducer/TodoItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const TodoItem = ({todo,onDeleteTodo}) => {
+  return (
+    <li  className="list-group-item d-flex justify-content-between">
+        <span className="aling-self-center">{todo.description}</span>
+        <button 
+          className="btn btn-danger"
+          onClick={()=>onDeleteTodo(todo.id)}
+        >
+        
+          Borrar
+        </button>
+    </li>
+  )
+}
---
+++ src/08-useReducer/index.js
@@ -0,0 +1,5 @@
+
+
+export * from './TodoList';
+export * from './TodoAdd';
+export * from './TodoReducer';
